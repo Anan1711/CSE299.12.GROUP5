b1ea9fa1eb4c44f1d32dac39ad02da4638ddee24
TalviaTasnim <60611235+TalviaTasnim@users.noreply.github.com>
2020-02-08 20:22:25 +0600

    MainCharacterAnimation

--- CSE299_Game_Project/Assets/PlayerMovement.cs
+++ CSE299_Game_Project/Assets/PlayerMovement.cs
@@ -10,7 +10,8 @@ public class PlayerMovement : MonoBehaviour
 	public float runSpeed = 40f;
 
 	float horizontalMove = 0f;
-
+	bool jump = false;
+	bool crouch = false;
 
 	// Update is called once per frame
 	void Update()
@@ -18,13 +19,26 @@ public class PlayerMovement : MonoBehaviour
 
 		horizontalMove = Input.GetAxisRaw("Horizontal") * runSpeed;
 
+		if (Input.GetButtonDown("Jump"))
+		{
+			jump = true;
+		}
+
+		if (Input.GetButtonDown("Crouch"))
+		{
+			crouch = true;
+		}
+		else if (Input.GetButtonUp("Crouch"))
+		{
+			crouch = false;
+		}
 
 	}
 
 	void FixedUpdate()
 	{
 		// Move our character
-		controller.Move(horizontalMove * Time.fixedDeltaTime, false, false);
-
+		controller.Move(horizontalMove * Time.fixedDeltaTime, crouch, jump);
+		jump = false;
 	}
 }
