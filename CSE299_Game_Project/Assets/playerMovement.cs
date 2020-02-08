849a1d303f88da41c916fa5d46b7d161a17b5608
Anan1711 <60573839+Anan1711@users.noreply.github.com>
2020-02-08 09:00:21 +0600

    Main Character 1
    Control design

---
+++ CSE299_Game_Project/Assets/playerMovement.cs
@@ -0,0 +1,42 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class playerMovement : MonoBehaviour
+{
+
+    public CharacterController2D controller;
+    public float runSpeed = 100f;
+    float horizontalMove = 0f;
+    bool Jump = false;
+    bool crouch = false;
+    
+
+    // Update is called once per frame
+    void Update()
+    {
+        horizontalMove = Input.GetAxisRaw("Horizontal")* runSpeed;
+
+        if (Input.GetButtonDown("Jump"))
+        {
+            Jump = true;
+        }
+
+        if (Input.GetButtonDown("Crouch"))
+        {
+            crouch = true;
+
+        }else if (Input.GetButtonUp("Crouch"))
+        {
+            crouch = false;
+        }
+        
+    }
+
+    void FixedUpdate()
+    {
+        // Move the character
+        controller.Move(horizontalMove * Time.fixedDeltaTime ,crouch,Jump);
+        Jump = false;
+    }
+}
