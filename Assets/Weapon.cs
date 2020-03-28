211f1bb96a2c54985fa48260074fa1e3becc45fe
TalviaTasnim <60611235+TalviaTasnim@users.noreply.github.com>
2020-03-28 23:28:52 +0600

    WEEK 6
    Shooting

--- Assets/Weapon.cs
+++ Assets/Weapon.cs
@@ -5,22 +5,49 @@ using UnityEngine;
 public class Weapon : MonoBehaviour
 {
     public Transform firePoint;
-    public GameObject bulletPrefab;
-
-
+    public int damage = 40;
+    public GameObject impactEffect;
+    public LineRenderer lineRenderer;
 
     // Update is called once per frame
     void Update()
     {
         if (Input.GetButtonDown("Fire1"))
         {
-            Shoot();
+           StartCoroutine(Shoot());
         }
     }
 
-    void Shoot()
+    IEnumerator Shoot()
     {
-        //shooting logic
-        Instantiate(bulletPrefab, firePoint.position, firePoint.rotation);
+        RaycastHit2D hitInfo = Physics2D.Raycast(firePoint.position, firePoint.right);
+
+        if (hitInfo)
+        {
+            Enemy enemy = hitInfo.transform.GetComponent<Enemy>();
+            if (enemy != null)
+            {
+                enemy.TakeDamage(damage);
+            }
+
+            Instantiate(impactEffect, hitInfo.point, Quaternion.identity);
+
+            lineRenderer.SetPosition(0, firePoint.position);
+            lineRenderer.SetPosition(1, hitInfo.point);
+        }
+        else
+        {
+            lineRenderer.SetPosition(0, firePoint.position);
+            lineRenderer.SetPosition(1, firePoint.position + firePoint.right * 100);
+        }
+
+        lineRenderer.enabled = true;
+
+        // wait one frame
+
+        yield return new WaitForSeconds(0.02f);
+
+
+        lineRenderer.enabled = false;
     }
 }
