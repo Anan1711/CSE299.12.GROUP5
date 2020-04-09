4586c889f2710a3f1cf4443fc6797a8da069f3bd
Anan1711 <60573839+Anan1711@users.noreply.github.com>
2020-04-09 13:51:38 +0600

    level Design v1.0.1

---
+++ CSE299_Game_Project/Assets/Mace.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Mace : MonoBehaviour
+{
+    private playerMovement knight;
+    int damageAmount = 10;
+    // Start is called before the first frame update
+    void Start()
+    {
+        knight = GameObject.FindGameObjectWithTag("Player").GetComponent<playerMovement>();
+        
+    }
+    
+    
+    void OnTriggerEnter2D(Collider2D col)
+    { 
+        
+        if(col.CompareTag("Player"))
+        {
+            knight.TakeDamage(damageAmount);
+        }
+    }
+   
+}
