d0dfe3f892fdeb5bd38eef91fe33d11394852529
TalviaTasnim <60611235+TalviaTasnim@users.noreply.github.com>
2020-04-10 23:20:31 +0600

    Week 7
    level design

---
+++ Assets/Platform.cs
@@ -0,0 +1,46 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Platform : MonoBehaviour
+{
+    private Vector3 posA;
+    private Vector3 posB;
+    private Vector3 nextPos;
+
+    [SerializeField]
+    private float speed;
+
+    [SerializeField]
+    private Transform child;
+
+    [SerializeField]
+    private Transform transformB;
+
+    void Start ()
+    {
+        posA = child.localPosition;
+        posB = transformB.localPosition;
+        nextPos = posB;
+    }
+
+    void Update ()
+    {
+        Move();
+    }
+
+    private void Move ()
+    {
+        child.localPosition = Vector3.MoveTowards(child.localPosition, nextPos, speed * Time.deltaTime);
+
+        if (Vector3.Distance(child.localPosition, nextPos) <= 0.1)
+        {
+            ChangeDest();
+        }
+    }
+
+    private void ChangeDest()
+    {
+        nextPos = nextPos != posA ? posA : posB;
+    }
+}
