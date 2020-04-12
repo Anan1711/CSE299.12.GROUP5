4115cbfe78a9c684bc32f3c0361408833f2df4af
Anan1711 <60573839+Anan1711@users.noreply.github.com>
2020-04-12 22:59:39 +0600

    Health Regeneration added

---
+++ CSE299_Game_Project/Assets/HealthBoost.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class HealthBoost : MonoBehaviour
+{
+    private playerMovement player;
+    private HealthBar healthbar;
+    public float healthBonus = 15f;
+
+    void Start()
+    {
+        player = GameObject.FindGameObjectWithTag("Player").GetComponent<playerMovement>();
+        healthbar = GameObject.FindGameObjectWithTag("HealthBar").GetComponent<HealthBar>();
+    }
+    void OnTriggerEnter2D(Collider2D col)
+    {
+        if(player.currentHealth < player.MaxHealth)
+        {
+            Destroy(gameObject);
+            player.currentHealth = player.currentHealth + healthBonus;
+            healthbar.SetHealth(player.currentHealth);
+            
+           
+        }
+    }
+}
