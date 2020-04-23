97e0f03f905b1fbae22db3e5cea3b767718b4c4a
MdAzizulHossain <46353891+MdAzizulHossain@users.noreply.github.com>
2020-04-23 18:26:22 +0600

    Falling Damage

---
+++ CSE299_Game_Project/Assets/FallingDamage.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FallingDamage : MonoBehaviour
+{
+    private PlayerMovement knight;
+    int damageAmount =999999;
+    // Start is called before the first frame update
+    void Start()
+    {
+        knight = GameObject.FindGameObjectWithTag("Player").GetComponent<PlayerMovement>();
+    }
+
+    void OnTriggerEnter2D(Collider2D col)
+    {
+
+        if (col.CompareTag("Player"))
+        {
+            knight.TakeDamage(damageAmount);
+        }
+    }
+}
