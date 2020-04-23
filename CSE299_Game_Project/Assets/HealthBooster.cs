e7c435757849783ed4cdebf6e680734564eda927
MdAzizulHossain <46353891+MdAzizulHossain@users.noreply.github.com>
2020-04-24 01:13:54 +0600

    Trap Added

---
+++ CSE299_Game_Project/Assets/HealthBooster.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class HealthBooster : MonoBehaviour
+{
+    private PlayerMovement player;
+    private HealthBar healthbar;
+    public float healthBonus = 15f;
+
+    void Start()
+    {
+        player = GameObject.FindGameObjectWithTag("Player").GetComponent<PlayerMovement>();
+        try
+        {
+            healthbar = GameObject.FindGameObjectWithTag("HealthBar").GetComponent<HealthBar>();
+        }
+        catch
+        {
+
+        }
+    }
+    void OnTriggerEnter2D(Collider2D col)
+    {
+        if (player.currentHealth < player.MaxHealth)
+        {
+            Destroy(gameObject);
+            player.currentHealth = player.currentHealth + healthBonus;
+            healthbar.SetHealth(player.currentHealth);
+
+
+        }
+    }
+}
