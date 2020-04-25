a871148b1c224d8798e1794f6cd9dfafbbcbbb4b
MdAzizulHossain <46353891+MdAzizulHossain@users.noreply.github.com>
2020-04-26 03:49:43 +0600

    Boss Death Effect

--- CSE299_Game_Project/Assets/PlayerCombat.cs
+++ CSE299_Game_Project/Assets/PlayerCombat.cs
@@ -12,7 +12,11 @@ public class PlayerCombat : MonoBehaviour
     public Transform attackPoint;
     public float attackRange = 0.5f;
 
-    public int attackDamage = 40;
+    public int attackDamage1;      //Blue
+    public int attackDamage2;      //Red
+    public int attackDamage3;      //Strike
+    public int attackDamage4;      //Ultra
+
     public float attackRate = 2f;
    // float nextAttackTime = 0f;
 
@@ -27,22 +31,22 @@ public class PlayerCombat : MonoBehaviour
         {
             Attack();
         }
-        if (Input.GetButtonDown("Fire3"))
-        {
-            Attack();
-        }
+       // if (Input.GetButtonDown("Fire3"))
+       // {
+        //    Attack();
+       // }
         if (Input.GetButtonDown("Fire4"))
         {
             Attack();
         }
         if (Input.GetButtonDown("Fire5"))
         {
-            Attack();
-        }
-        if (Input.GetButtonDown("Fire6"))
-        {
-            Attack();
-        }
+           Attack();
+      }
+     //  if (Input.GetButtonDown("Fire6"))
+       // {
+       //    Attack();
+       // }
     }
     void Attack()
     {
@@ -56,10 +60,10 @@ public class PlayerCombat : MonoBehaviour
         {
             animator.SetTrigger("Attack2");
         }
-        else if (Input.GetButtonDown("Fire3"))
-        {
-            animator.SetTrigger("JumpAttack");
-        }
+       // else if (Input.GetButtonDown("Fire3"))
+       // {
+         //   animator.SetTrigger("JumpAttack");
+       // }
         else if (Input.GetButtonDown("Fire4"))
         {
             animator.SetTrigger("Strike");
@@ -78,27 +82,71 @@ public class PlayerCombat : MonoBehaviour
         {
             try
             {
-                enemy.GetComponent<EnemyAl_1>().TakeDamage(attackDamage);
-
+                if(Input.GetButtonDown("Fire1"))
+                { 
+                enemy.GetComponent<EnemyAl_1>().TakeDamage(attackDamage1);
+                }
+                else if(Input.GetButtonDown("Fire2"))
+                {
+                    enemy.GetComponent<EnemyAl_1>().TakeDamage(attackDamage2);
+                }
+                else if (Input.GetButtonDown("Fire4"))
+                {
+                    enemy.GetComponent<EnemyAl_1>().TakeDamage(attackDamage3);
+                }
+                else if (Input.GetButtonDown("Fire5"))
+                {
+                    enemy.GetComponent<EnemyAl_1>().TakeDamage(attackDamage4);
+                }
             }
             catch
             {
                 // Debug.LogError("Something is wrong 1");
             }
+
             try
             {
-                enemy.GetComponent<EnemyAI_2>().TakeDamage(attackDamage);
-
+                if (Input.GetButtonDown("Fire1"))
+                {
+                    enemy.GetComponent<EnemyAI_2>().TakeDamage(attackDamage1);
+                }
+                else if (Input.GetButtonDown("Fire2"))
+                {
+                    enemy.GetComponent<EnemyAI_2>().TakeDamage(attackDamage2);
+                }
+                else if (Input.GetButtonDown("Fire4"))
+                {
+                    enemy.GetComponent<EnemyAI_2>().TakeDamage(attackDamage3);
+                }
+                else if (Input.GetButtonDown("Fire5"))
+                {
+                    enemy.GetComponent<EnemyAI_2>().TakeDamage(attackDamage4);
+                }
             }
             catch
             {
                 // Debug.LogError("Something is wrong 2");
             }
 
+
             try
             {
-                enemy.GetComponent<EnemyAI_3>().TakeDamage(attackDamage);
-
+                if (Input.GetButtonDown("Fire1"))
+                {
+                    enemy.GetComponent<EnemyAI_3>().TakeDamage(attackDamage1);
+                }
+                else if (Input.GetButtonDown("Fire2"))
+                {
+                    enemy.GetComponent<EnemyAI_3>().TakeDamage(attackDamage2);
+                }
+                else if (Input.GetButtonDown("Fire4"))
+                {
+                    enemy.GetComponent<EnemyAI_3>().TakeDamage(attackDamage3);
+                }
+                else if (Input.GetButtonDown("Fire5"))
+                {
+                    enemy.GetComponent<EnemyAI_3>().TakeDamage(attackDamage4);
+                }
             }
             catch
             {
@@ -107,8 +155,22 @@ public class PlayerCombat : MonoBehaviour
 
             try
             {
-                enemy.GetComponent<EnemyAI_4>().TakeDamage(attackDamage);
-
+                if (Input.GetButtonDown("Fire1"))
+                {
+                    enemy.GetComponent<EnemyAI_4>().TakeDamage(attackDamage1);
+                }
+                else if (Input.GetButtonDown("Fire2"))
+                {
+                    enemy.GetComponent<EnemyAI_4>().TakeDamage(attackDamage2);
+                }
+                else if (Input.GetButtonDown("Fire4"))
+                {
+                    enemy.GetComponent<EnemyAI_4>().TakeDamage(attackDamage3);
+                }
+                else if (Input.GetButtonDown("Fire5"))
+                {
+                    enemy.GetComponent<EnemyAI_4>().TakeDamage(attackDamage4);
+                }
             }
             catch
             {
@@ -117,18 +179,47 @@ public class PlayerCombat : MonoBehaviour
 
             try
             {
-                enemy.GetComponent<EnemyAI_5>().TakeDamage(attackDamage);
-
+                if (Input.GetButtonDown("Fire1"))
+                {
+                    enemy.GetComponent<EnemyAI_5>().TakeDamage(attackDamage1);
+                }
+                else if (Input.GetButtonDown("Fire2"))
+                {
+                    enemy.GetComponent<EnemyAI_5>().TakeDamage(attackDamage2);
+                }
+                else if (Input.GetButtonDown("Fire4"))
+                {
+                    enemy.GetComponent<EnemyAI_5>().TakeDamage(attackDamage3);
+                }
+                else if (Input.GetButtonDown("Fire5"))
+                {
+                    enemy.GetComponent<EnemyAI_5>().TakeDamage(attackDamage4);
+                }
             }
             catch
             {
                 // Debug.LogError("Something is wrong 5");
             }
 
+
             try
             {
-                enemy.GetComponent<EnemyAI_6>().TakeDamage(attackDamage);
-
+                if (Input.GetButtonDown("Fire1"))
+                {
+                    enemy.GetComponent<EnemyAI_6>().TakeDamage(attackDamage1);
+                }
+                else if (Input.GetButtonDown("Fire2"))
+                {
+                    enemy.GetComponent<EnemyAI_6>().TakeDamage(attackDamage2);
+                }
+                else if (Input.GetButtonDown("Fire4"))
+                {
+                    enemy.GetComponent<EnemyAI_6>().TakeDamage(attackDamage3);
+                }
+                else if (Input.GetButtonDown("Fire5"))
+                {
+                    enemy.GetComponent<EnemyAI_6>().TakeDamage(attackDamage4);
+                }
             }
             catch
             {
@@ -137,8 +228,22 @@ public class PlayerCombat : MonoBehaviour
 
             try
             {
-                enemy.GetComponent<EnemyAI_7>().TakeDamage(attackDamage);
-
+                if (Input.GetButtonDown("Fire1"))
+                {
+                    enemy.GetComponent<EnemyAI_7>().TakeDamage(attackDamage1);
+                }
+                else if (Input.GetButtonDown("Fire2"))
+                {
+                    enemy.GetComponent<EnemyAI_7>().TakeDamage(attackDamage2);
+                }
+                else if (Input.GetButtonDown("Fire4"))
+                {
+                    enemy.GetComponent<EnemyAI_7>().TakeDamage(attackDamage3);
+                }
+                else if (Input.GetButtonDown("Fire5"))
+                {
+                    enemy.GetComponent<EnemyAI_7>().TakeDamage(attackDamage4);
+                }
             }
             catch
             {
@@ -147,8 +252,22 @@ public class PlayerCombat : MonoBehaviour
 
             try
             {
-                enemy.GetComponent<EnemyAI_8>().TakeDamage(attackDamage);
-
+                if (Input.GetButtonDown("Fire1"))
+                {
+                    enemy.GetComponent<EnemyAI_8>().TakeDamage(attackDamage1);
+                }
+                else if (Input.GetButtonDown("Fire2"))
+                {
+                    enemy.GetComponent<EnemyAI_8>().TakeDamage(attackDamage2);
+                }
+                else if (Input.GetButtonDown("Fire4"))
+                {
+                    enemy.GetComponent<EnemyAI_8>().TakeDamage(attackDamage3);
+                }
+                else if (Input.GetButtonDown("Fire5"))
+                {
+                    enemy.GetComponent<EnemyAI_8>().TakeDamage(attackDamage4);
+                }
             }
             catch
             {
@@ -157,22 +276,50 @@ public class PlayerCombat : MonoBehaviour
 
             try
             {
-                enemy.GetComponent<EnemyAI_9>().TakeDamage(attackDamage);
-
+                if (Input.GetButtonDown("Fire1"))
+                {
+                    enemy.GetComponent<EnemyAI_9>().TakeDamage(attackDamage1);
+                }
+                else if (Input.GetButtonDown("Fire2"))
+                {
+                    enemy.GetComponent<EnemyAI_9>().TakeDamage(attackDamage2);
+                }
+                else if (Input.GetButtonDown("Fire4"))
+                {
+                    enemy.GetComponent<EnemyAI_9>().TakeDamage(attackDamage3);
+                }
+                else if (Input.GetButtonDown("Fire5"))
+                {
+                    enemy.GetComponent<EnemyAI_9>().TakeDamage(attackDamage4);
+                }
             }
             catch
             {
-                // Debug.LogError("Something is wrong 9");
+                // Debug.LogError("Something is wrong 2");
             }
+
             try
             {
-                enemy.GetComponent<BossHealth>().TakeDamage(attackDamage);
-
+                if (Input.GetButtonDown("Fire1"))
+                {
+                    enemy.GetComponent<BossHealth>().TakeDamage(attackDamage1);
+                }
+                else if (Input.GetButtonDown("Fire2"))
+                {
+                    enemy.GetComponent<BossHealth>().TakeDamage(attackDamage2);
+                }
+                else if (Input.GetButtonDown("Fire4"))
+                {
+                    enemy.GetComponent<BossHealth>().TakeDamage(attackDamage3);
+                }
+                else if (Input.GetButtonDown("Fire5"))
+                {
+                    enemy.GetComponent<BossHealth>().TakeDamage(attackDamage4);
+                }
             }
             catch
             {
-                // Debug.LogError("Something is wrong boss
-               
+                // Debug.LogError("Something is wrong boss");
             }
 
           
