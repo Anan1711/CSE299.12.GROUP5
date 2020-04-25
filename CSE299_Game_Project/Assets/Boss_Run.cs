472b72c13c173bb343e0eb870edf3b3f4187cabd
Anan1711 <60573839+Anan1711@users.noreply.github.com>
2020-04-25 12:23:16 +0600

    Pause Menu added

--- CSE299_Game_Project/Assets/Boss_Run.cs
+++ CSE299_Game_Project/Assets/Boss_Run.cs
@@ -25,11 +25,12 @@ public class Boss_Run : StateMachineBehaviour
 	override public void OnStateUpdate(Animator animator, AnimatorStateInfo stateInfo, int layerIndex)
 	{
 		boss.LookAtPlayer();
-
-		Vector2 target = new Vector2(player.position.x, rb.position.y);
-		Vector2 newPos = Vector2.MoveTowards(rb.position, target, speed * Time.fixedDeltaTime);
-		rb.MovePosition(newPos);
-
+		
+		
+	     Vector2 target = new Vector2(player.position.x, rb.position.y);
+	     Vector2 newPos = Vector2.MoveTowards(rb.position, target, speed * Time.fixedDeltaTime);
+	     rb.MovePosition(newPos);
+		
 		if (Vector2.Distance(player.position, rb.position) <= attackRange)
 		{
 			animator.SetTrigger("Attack");
