b634d0049e913cc8269eed88fa60711e64386c08
MdAzizulHossain <46353891+MdAzizulHossain@users.noreply.github.com>
2020-04-26 01:12:10 +0600

    Add Menu & Pause Button

---
+++ CSE299_Game_Project/Assets/MainMenu.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class MainMenu : MonoBehaviour
+{
+    public void PlayGame()
+    {
+        // loading the next scene in the queue by index number.
+        SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex + 1);
+    }
+    public void QuitGame()
+    {
+        // Just print an Quit statement. No actually closing.
+        Debug.Log("Quit!");
+        Application.Quit();
+    }
+}
